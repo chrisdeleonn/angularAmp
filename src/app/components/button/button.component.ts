5cbfd270319d6a3ac58808459ab2c7ced9568956
Chris De Leon <cdeleon.ca91@gmail.com>
2021-11-05 23:03:56 -0400

    fixed ts file

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -8,10 +8,12 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ButtonComponent implements OnInit {
   @Input() text: string;
   @Input() color: string;
+  @Output() btnClick = new EventEmitter();
+
   constructor() {}
 
   ngOnInit(): void {}
   onClick() {
-    console.log('add');
+    this.btnClick.emit();
   }
 }
